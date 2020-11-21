da716dabc6822a76634c0492f26bab17a1d46ef2
Rene Sauerwein <rene@sauerwein.cc>
2020-11-21 21:33:30 +0100

    Code cleanup

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,10 +1,6 @@
 const eMailForm = document.querySelector(".signup__email");
 const errorMessage = document.querySelector(".signup__email--error")
 
-function insertAfter(newNode, referenceNode) {
-  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
 eMailForm.addEventListener("invalid", function(e) {
   errorMessage.style.display = "block";
   console.log(errorMessage);
