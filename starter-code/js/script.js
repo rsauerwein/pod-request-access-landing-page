3c41c579bc2616f6e085ccfad774f24238d761d5
Rene Sauerwein <rene@sauerwein.cc>
2020-11-21 21:33:10 +0100

    Bugfix - the email validation error now only appears once

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,13 +1,12 @@
 const eMailForm = document.querySelector(".signup__email");
+const errorMessage = document.querySelector(".signup__email--error")
 
 function insertAfter(newNode, referenceNode) {
   referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 eMailForm.addEventListener("invalid", function(e) {
-  const error = document.createElement("p");
-  error.classList.add("signup__email--error");
-  error.innerText = `Oops! Please check your email`;
-  insertAfter(error, e.target);
+  errorMessage.style.display = "block";
+  console.log(errorMessage);
   e.preventDefault();
 })
