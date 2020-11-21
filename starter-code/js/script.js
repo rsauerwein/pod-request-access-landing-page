1975806ad561d83b09b62225626842cc179e8b3d
Rene Sauerwein <rene@sauerwein.cc>
2020-11-21 21:33:42 +0100

    Code cleanup

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -3,6 +3,5 @@ const errorMessage = document.querySelector(".signup__email--error")
 
 eMailForm.addEventListener("invalid", function(e) {
   errorMessage.style.display = "block";
-  console.log(errorMessage);
   e.preventDefault();
 })
